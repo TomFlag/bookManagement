b116e995e86abab9e1186354c4623ea0f68fdb19
TomFlag <qweasdzxc.tomoki.10969@gmail.com>
2025-10-27 00:41:43 +0900

    Spring Initializrでプロジェクト作成

---
+++ src/test/kotlin/com/hata/bookManagement/BookManagementApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hata.bookManagement
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookManagementApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
